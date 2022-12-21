51db320b403c7644948fa6d3afaa186000497536
GhUserrrr <101joe.prime@gmail.com>
2022-12-21 19:29:38 +0300

    added v-click-outside

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import { createApp } from "vue";
 import "./assets/scss/main.scss";
 import { createPinia } from "pinia";
 import App from "./App.vue";
-import router from './router'
+import router from "./router";
+import vClickOutside from "click-outside-vue3";
 
 const pinia = createPinia();
 const app = createApp(App).use(router);
 
+app.use(vClickOutside);
 app.use(pinia);
 app.mount("#app");
